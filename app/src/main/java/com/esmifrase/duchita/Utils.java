14e61e8b758d8465a0d42873ddfd97169075c725
Yizack <yizackr@gmail.com>
2019-01-08 02:36:46 -0500

    Update Utils.java

--- app/src/main/java/com/esmifrase/duchita/Utils.java
+++ app/src/main/java/com/esmifrase/duchita/Utils.java
@@ -4,5 +4,5 @@ public class Utils {
     public static final String BASE_IP          = "https://dimatis.yizack.com/";
     public static final String LOGIN_URL        = BASE_IP + "db_requests/ducha/login.php";
     public static final String REGISTER_URL     = BASE_IP + "db_requests/ducha/register.php";
-    public static final String UPDATE_URL           = BASE_IP + "db_requests/ducha/update.php";
+    public static final String UPDATE_URL       = BASE_IP + "db_requests/ducha/update.php";
 }
